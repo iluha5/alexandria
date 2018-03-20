51f9cfa929af3316c98838bdf6ad678f1724245f
iluha5 <ilya.rovda@gmail.com>
2018-03-20 21:51:54 +0300

    Downgrade for gulp-less to v3.5.0. v4.0.0 wasn't work

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@
 var gulp = require('gulp'); // Подключаем Gulp
 var less = require('gulp-less'); //Подключаем less пакет,
 var browserSync = require('browser-sync'); // Подключаем Browser Sync
+var server = require("browser-sync").create(); // подключаем еще один browser-sync
 var path = require('path');
-var plumder = require('gulp-plumber');
+var plumber = require('gulp-plumber');
 var postcss = require('gulp-postcss');
 var autoprefixer = require("autoprefixer");
 var mqpacker = require('css-mqpacker');
@@ -16,35 +17,37 @@ var svgstore = require("gulp-svgstore");
 var run = require('run-sequence');
 var del = require('del');
 
-//////////////////////////////////////////////////////////
+/////////////////////////// Не рабочие варианты ///////////////////////////////
 
-gulp.task('less', ['less3'], function () { // Создаем таск less
-    return gulp.src('less/style.less') // Берем источник
-    //        .pipe(less([ path.join(__dirname, 'less', 'includes') ])) // Преобразуем less в CSS посредством gulp-less
-    //        .pipe(gulp.dest('css')) // Выгружаем результата в папку css
-        .pipe(browserSync.reload({stream: true})); // Обновляем CSS на странице при изменении
-});
+// gulp.task('less', ['less3'], function () { // Создаем таск less
+//     return gulp.src('less/style.less') // Берем источник
+//     //        .pipe(less([ path.join(__dirname, 'less', 'includes') ])) // Преобразуем less в CSS посредством gulp-less
+//     //        .pipe(gulp.dest('css')) // Выгружаем результата в папку css
+//         .pipe(browserSync.reload({stream: true})); // Обновляем CSS на странице при изменении
+// });
+//
+// gulp.task('browser-sync', function () { // Создаем таск browser-sync
+//     browserSync({ // Выполняем browserSync
+//         server: { // Определяем параметры сервера
+//             baseDir: '.' // Директория для сервера - .
+//         },
+//         notify: false // Отключаем уведомления
+//     });
+// });
+//
+// gulp.task('watch', ['less', 'browser-sync'], function () {
+//     gulp.watch('less/**/*.less', ['less']); // Наблюдение за less файлами
+//     // Наблюдение за другими типами файлов
+// });
+///////////////////////////////////////////////////////////////////
 
-gulp.task('browser-sync', function () { // Создаем таск browser-sync
-    browserSync({ // Выполняем browserSync
-        server: { // Определяем параметры сервера
-            baseDir: '.' // Директория для сервера - .
-        },
-        notify: false // Отключаем уведомления
-    });
-});
-
-gulp.task('watch', ['less', 'browser-sync'], function () {
-    gulp.watch('less/**/*.less', ['less']); // Наблюдение за less файлами
-    // Наблюдение за другими типами файлов
-});
 
 
 ///////// РАБОТА С CSS /////////////////////////////
 
 gulp.task('less3', function () {
     return gulp.src('less/style.less')
-        .pipe(plumder())
+        .pipe(plumber())
         .pipe(less([path.join(__dirname, 'less')])) // Преобразуем less в CSS посредством gulp-less
         .pipe(postcss([
             autoprefixer({
@@ -99,14 +102,14 @@ gulp.task('symbols', function () {
 //////////////////// ВОТЧЕРЫ И СИНХРОНИЗАЦИЯ С БРАУЗЕРОМ ////////////////////
 // еще одна функция для browser-sync :)
 
-gulp.task("serve", ["less3"], function () {
-    browserSync.init({
-        server: "."
-    });
-
-    gulp.watch("less/**/*.less", ["less3"]);
-    gulp.watch("*.html").on("change", browserSync.reload);
-});
+// gulp.task("serve", ["less3"], function () {
+//     browserSync.init({
+//         server: "."
+//     });
+//
+//     gulp.watch("less/**/*.less", ["less3"]);
+//     gulp.watch("*.html").on("change", browserSync.reload);
+// });
 
 ////////////////////////////////////////////////////////////////////////////
 
@@ -143,3 +146,39 @@ gulp.task('build', function (fn){
 });
 
 ///////////////////////////////////////////////////////////////////////////
+
+/////////////// TЩЕ ОДИН ВОТЧЕР //////////////////////////
+
+gulp.task("style1", function() {
+    gulp.src("less/style.less")
+        .pipe(plumber())
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(postcss([
+            autoprefixer({
+                browsers: [
+                    "last 1 version",
+                    "last 2 Chrome versions",
+                    "last 2 Firefox versions",
+                    "last 2 Opera versions",
+                    "last 2 Edge versions"
+                ]
+            })
+        ]))
+        .pipe(gulp.dest("css"))
+        .pipe(server.stream());
+});
+
+gulp.task("serve1", ["style1"], function() {
+    server.init({
+        server: ".",
+        notify: false,
+        open: true,
+        cors: true,
+        ui: false
+    });
+
+    gulp.watch("less/**/*.less", ["style1"]);
+    gulp.watch("*.html").on("change", server.reload);
+});
